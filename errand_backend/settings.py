2ca6f38ca22cbe824e3c88c2dae9e67d719e65d7
Michael Ampomah <ampomahmichael321@gmail.com>
2025-12-30 23:27:25 +0000

    added the admin netlify site to cors_allowed_origins

--- errand_backend/settings.py
+++ errand_backend/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = ['https://app-errand.netlify.app']
+CORS_ALLOWED_ORIGINS = ['https://app-errand.netlify.app','https://admin-errand-app.netlify.app']
 
 ROOT_URLCONF = 'errand_backend.urls'
 
